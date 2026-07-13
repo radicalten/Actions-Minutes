434ac4579fea4f4e9b455c5544786ca8adcd57dd
radicalten <79894181+radicalten@users.noreply.github.com>
2026-07-12 19:02:01 -0500

    Update wii_video.h

--- noods-wii-9-5-clean7/wii_video.h
+++ noods-wii-9-5-clean7/wii_video.h
@@ -1,22 +1,3 @@
-/*
-    Copyright (C) 2026 radicalten
-
-    This file is part of NooDS-Wii.
-
-    NooDS-Wii is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    NooDS-Wii is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with NooDS-Wii. If not, see <https://www.gnu.org/licenses/>.
-*/
-
 #ifndef WII_VIDEO_H
 #define WII_VIDEO_H
 
@@ -51,8 +32,11 @@ struct WiiDebugOverlay {
 };
 
 void Wii_VideoInit();
-void Wii_VideoRender(const uint32_t* srcTop, const uint32_t* srcBottom,
+
+// Takes 16-bit pointers for direct uploading
+void Wii_VideoRender(const uint16_t* srcTop, const uint16_t* srcBottom,
                      bool gbaMode = false);
+
 void Wii_VideoFlushAsync();
 void Wii_DrawCursor(float x, float y);
 void Wii_DebugOverlayInit();
