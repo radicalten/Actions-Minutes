e38f84d94bed359322bfc0496724248030b0243b
radicalten <79894181+radicalten@users.noreply.github.com>
2026-07-16 18:26:11 -0500

    Update jit_ppc.h

--- backup4-prod-profile-2/test/cson4pt6-jitv3/jit_ppc.h
+++ backup4-prod-profile-2/test/cson4pt6-jitv3/jit_ppc.h
@@ -2,10 +2,11 @@
 // jit_ppc.h (inline header section)
 // ============================================================
 // In a separate jit_ppc.h file include:
-/*
+
 #pragma once
 #include <cstdint>
 #include <cstdlib>
+#include "core.h"
 
 namespace JitPpc {
     bool initJit();
