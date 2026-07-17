d66c93ca506638ed55e79597afeee8459c6b24ff
radicalten <79894181+radicalten@users.noreply.github.com>
2026-07-17 17:36:59 -0500

    Update jit_ppc.h

--- backup4-prod-profile-2/jit_ppc.h
+++ backup4-prod-profile-2/jit_ppc.h
@@ -1,10 +1,18 @@
+// jit_ppc.h
 #pragma once
 #include <cstdint>
 
 class Core;
 
-// Declared here so jit_ppc.cpp can use it; defined in jit_trampoline.S
-extern "C" void executeBlock_asm(uint32_t* blockCode);
+// Defined in jit_trampoline.S
+// Calls blockCode via bctrl so JIT block's mflr gets a valid PPC return address
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void executeBlock_asm(uint32_t* blockCode);
+#ifdef __cplusplus
+}
+#endif
 
 namespace JitPpc {
     bool initJit(Core* core);
