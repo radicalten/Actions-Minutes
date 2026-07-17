3c0a7a51917c8c8a464cd7bf4869f4c645594c77
radicalten <79894181+radicalten@users.noreply.github.com>
2026-07-17 13:48:52 -0500

    Update core.h

--- backup4-prod-profile-2/core.h
+++ backup4-prod-profile-2/core.h
@@ -196,9 +196,10 @@ public:
     // Hot path: single load + indirect branch, no virtual dispatch.
     inline void runCore() { (*runFunc)(*this); }
 
+
     void setRunFunc(void (*f)(Core&)) {
-    runFunc = f ? f : &Interpreter::runCoreNds;
-}
+    runFunc = (f != nullptr) ? f : &Interpreter::runCoreNds;
+    }
 
     void schedule(SchedTask task, uint32_t cycles);
     void enterGbaMode();
