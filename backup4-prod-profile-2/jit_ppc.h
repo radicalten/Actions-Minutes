971c54baeef43007c75e9f10427342426094b7a8
radicalten <79894181+radicalten@users.noreply.github.com>
2026-07-17 17:11:24 -0500

    Update jit_ppc.h

--- backup4-prod-profile-2/jit_ppc.h
+++ backup4-prod-profile-2/jit_ppc.h
@@ -1,17 +1,14 @@
-// jit_ppc.h
 #pragma once
 #include <cstdint>
-#include <cstdlib>
 
 class Core;
+
+// Declared here so jit_ppc.cpp can use it; defined in jit_trampoline.S
+extern "C" void executeBlock_asm(uint32_t* blockCode);
 
 namespace JitPpc {
     bool initJit(Core* core);
     void shutdownJit(Core* core);
-
-    inline bool initJit()     { return initJit(nullptr);  }
-    inline void shutdownJit() { shutdownJit(nullptr);     }
-
     void runJitNds(Core& core);
     void runJitGba(Core& core);
     void invalidateJitRange(uint32_t start, uint32_t end);
